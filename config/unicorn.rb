18f111ac68555d6bfd6c959a40276a997021cab1
Mike Shutt <mjshutt@gmail.com>
2014-04-28 11:35:49 -0400

    more config changes for heroku

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -14,12 +14,15 @@ before_fork do |server, worker|
 end
 
 after_fork do |server, worker|
+
   Signal.trap 'TERM' do
     puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
   end
 
-  defined?(ActiveRecord::Base) and
+  if defined?(ActiveRecord::Base)
     config = Rails.application.config.database_configuration[Rails.env]
     config['adapter'] = 'postgis'
-    ActiveRecord::Base.establish_connection
-end
+    ActiveRecord::Base.establish_connection(config)
+  end
+
+endend
