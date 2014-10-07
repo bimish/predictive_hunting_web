0d4044fe13f0a1a635b43f963f485f1a6b3c7f8e
Mike Shutt <mjshutt@gmail.com>
2014-10-07 15:33:51 -0400

    precompiled assets issue

--- app/assets/javascripts/hunting_app_shared.js
+++ app/assets/javascripts/hunting_app_shared.js
@@ -10,27 +10,3 @@ function callPageInitializers(event) {
     }
   );
 }
-
-MapIcons.HuntingLocationAvailable = {
-  url: '/assets/map_marker_green.png',
-  size: new google.maps.Size(16,16),
-  origin: new google.maps.Point(0, 0),
-  anchor: new google.maps.Point(8, 8),
-  scaledSize: new google.maps.Size(16,16)
-};
-
-MapIcons.HuntingLocationReserved = {
-  url: '/assets/map_marker_yellow.png',
-  size: new google.maps.Size(16,16),
-  origin: new google.maps.Point(0, 0),
-  anchor: new google.maps.Point(8, 8),
-  scaledSize: new google.maps.Size(16,16)
-};
-
-MapIcons.HuntingLocationOccupied = {
-  url: '/assets/map_marker_red.png',
-  size: new google.maps.Size(16,16),
-  origin: new google.maps.Point(0, 0),
-  anchor: new google.maps.Point(8, 8),
-  scaledSize: new google.maps.Size(16,16)
-};
