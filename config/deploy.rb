8e69b13d49781fd27d272ab2fc0d4188aa69f81c
Mike Shutt <mjshutt@gmail.com>
2015-09-28 16:17:58 -0400

    capistrano config changes

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # Change these
-server '172.31.27.184', port: 22, roles: [:web, :app, :db], primary: true
+server '54.68.72.198', port: 22, roles: [:web, :app, :db], primary: true, user:'mike'
 
-set :repo_url,        'github.com/bimish/predictive_hunting_web.git'
+set :repo_url,        'https://github.com/bimish/predictive_hunting_web.git'
 set :application,     'predictive_hunting_web'
 set :user,            'mike'
 set :puma_threads,    [4, 16]
@@ -18,7 +18,8 @@ set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
 set :puma_error_log,  "#{release_path}/log/puma.access.log"
-set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+#set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w('~/.ssh/id_rsa.pub'), :verbose => :debug }
+set :ssh_options,     { use_agent: false, user: 'mike', :verbose => :debug }
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
