579bb0d48d7bd75228c254b6ba3d8ddc6eb551d6
Mike Shutt <mjshutt@gmail.com>
2015-09-28 20:33:52 -0400

    capistrano tweaks

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,7 +19,7 @@ set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
 set :puma_error_log,  "#{release_path}/log/puma.access.log"
 #set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w('~/.ssh/id_rsa.pub'), :verbose => :debug }
-set :ssh_options,     { use_agent: false, user: 'mike', :verbose => :debug }
+set :ssh_options,     { use_agent: false, user: 'mike' }
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
@@ -52,8 +52,8 @@ namespace :deploy do
   desc "Make sure local git is in sync with remote."
   task :check_revision do
     on roles(:app) do
-      unless `git rev-parse HEAD` == `git rev-parse origin/master`
-        puts "WARNING: HEAD is not the same as origin/master"
+      unless `git rev-parse HEAD` == `git rev-parse origin/#{fetch(:branch)}`
+        puts "WARNING: HEAD is not the same as origin/#{fetch(:branch)}"
         puts "Run `git push` to sync changes."
         exit
       end
